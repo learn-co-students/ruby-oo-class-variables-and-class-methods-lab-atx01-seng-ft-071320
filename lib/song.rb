1ab76a2cc8283ba23991d42c885dd149384da139
Ceddyp19 <ceddyp117@gmail.com>
2020-07-16 16:49:13 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,57 @@
+require 'pry'
+class Song 
+ attr_accessor :name, :genre, :artist 
+
+
+ @@count=0 
+ @@genres=[] 
+ @@artists=[] 
+
+   def initialize (name, artist, genre)
+    @name=name
+    @genre=genre
+    @artist=artist
+    @@count+=1
+    @@genres << genre 
+    @@artists << artist 
+   end
+
+   def self.count
+    @@count 
+   end
+
+   
+   def self.genres
+    @@genres.uniq 
+   end 
+     
+
+   def self.artists
+    @@artists.uniq 
+   end
+
+   def self.genre_count 
+        hash={}
+        @@genres.each do |genre|
+            if hash[genre]==nil 
+                hash[genre]=1 
+            else
+                hash[genre]+=1
+            end 
+        end   
+        hash    
+     end 
+
+     def self.artist_count 
+        hash={}
+        @@artists.each do |artist|
+            if hash[artist]==nil 
+                hash[artist]=1 
+            else
+                hash[artist]+=1
+            end 
+        end   
+        hash    
+     end 
+
+end
