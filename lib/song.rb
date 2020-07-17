6f7085275e98bd93ff5606744394942bfe333f0d
jtskidmore <johntoddskidmore@gmail.com>
2020-07-16 21:17:11 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+require "pry"
+
+class Song
+
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+
+  @@artists = []
+
+  @@genres = []
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << @artist
+    @genre = genre
+    @@genres << @genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each do |genre|
+      genre_hash[genre] += 1
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = Hash.new(0)
+    @@artists.each do |artist|
+      artist_hash[artist] += 1
+    end
+    artist_hash
+  end
+
+end
